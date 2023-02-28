45bb54c51fa5f3af31926ebd4e321477624c292f
Nicolas Cort <110467001+NicolasCort@users.noreply.github.com>
2023-02-28 13:36:22 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,67 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ML Agent App",
+    page_icon=":robot_face:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    background_color="#e6f2ff"
+    )
+
+
+
+st.write("# Machine Learning Agent 🤖")
+
+
+st.write("""
+    👋 Hey there, folks! Are you tired of manually analyzing data, making charts, and trying to predict the future 🤔? Fear not, because we've got just the thing for you: a machine learning agent that will do all the heavy lifting for you! 🤖💪
+
+Our super-smart AI buddy has the ability to create a study of any data set you give it 📊. That means no more late nights staring at spreadsheets or squinting at tiny font sizes 🙅‍♀️. Just sit back, relax, and let the machine learning agent work its magic 🔮.
+
+Not only that, but this amazing virtual friend can also use the data to create a model that will predict future outcomes with incredible accuracy 🎯. No more guesswork or relying on a Magic 8 Ball 🎱. Our machine learning agent will use the latest and greatest algorithms to analyze the data and make predictions that will blow your mind 🤯.
+
+But wait, there's more! Our machine learning agent is not just a boring number-cruncher 🙅‍♂️. 
+
+So why waste time and energy analyzing data when you can have your own personal machine learning agent do it for you? 🤖💻 Say goodbye to spreadsheets and hello to an easier, more fun way of predicting the future 🚀!
+""")
+
+if st.button('How to navigate trough the web'):
+    #st.markdown('## Information')
+    st.write("""
+
+First things first, we've got two pages for you to explore: one for Supervised ML 🕵️‍♀️ and one for Unsupervised ML 🕵️‍♂️. Supervised ML is like having a wise old owl 🦉 teach you things, while Unsupervised ML is like going on a treasure hunt 🗺️ without any clues. Sounds exciting, right?
+
+Here's how to move around our web:
+
+1️⃣ Choose the Supervised page
+
+2️⃣ Once you're there, take a moment to appreciate the sleek design of our web. It's like looking at a work of art 🎨, except it's also super functional!
+
+3️⃣ On the Supervised ML page, input some data 📊 and our ML agent will create a profiling and model for prediction. It's like having a magic wand 🧙, except it's science!
+
+4️⃣ And that's it! You're now a pro at navigating our web. Wasn't that easy? Now go forth and explore the exciting world of Machine Learning! Who knows, you might just discover something amazing! 🚀
+
+So what are you waiting for? Hop on over to our web and start exploring! 🤠
+    
+    
+""")
+
+if st.button('Who is the guy behind this app 👨'):
+    #st.markdown('## Information')
+    st.write("""
+    🤔 Wondering who's behind all these witty ? Well, let me introduce you to the genius behind the screen:
+
+    👨‍💻 Meet the author, a skilled wordsmith and a master of all things ! 
+
+    📲 If you want to keep up with the author, make sure to follow him on social media! Here are some handy links:
+
+    💻 GitHub: [https://github.com/NicolasCort]
+
+    🎥 Youtube: [https://www.youtube.com/@nicocmw8501]
+
+    👍 Don't miss out on all the fun - give the author a follow today! 😄
+    
+""")
+
+
+
